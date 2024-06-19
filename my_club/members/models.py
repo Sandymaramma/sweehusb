1fbee58b9ee35e5bc92c73539d26e1645c27458c
sandymaramma <edesandraoge@gmail.com>
2024-06-19 06:44:55 +0100

    updates

--- my_club/members/models.py
+++ my_club/members/models.py
@@ -4,4 +4,4 @@ class Member(models.Model):
   firstname = models.CharField(max_length=225)
   lastname = models.CharField(max_length=225)
   phone = models.IntegerField()
-  joined_date = models.DateField()
+  joined_date = models.DateField()(default=date.today)
