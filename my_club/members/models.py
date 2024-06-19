e6e1913218a42b9f64419a627b9a36f04d9335c8
sandymaramma <edesandraoge@gmail.com>
2024-06-19 12:49:59 +0100

    updates

--- my_club/members/models.py
+++ my_club/members/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from datetime import date
 
 class Member(models.Model):
   firstname = models.CharField(max_length=225)
   lastname = models.CharField(max_length=225)
   phone = models.IntegerField()
-  joined_date = models.DateField()(default=date.today)
+  joined_date = models.DateField(default=date.today)
